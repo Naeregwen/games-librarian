7408fbe11bfb241acb8e3f22863cf23cc4f2d4fe
alex.nguyenvan <alex.nguyenvan@gmail.com>
2014-09-07 20:35:42 +0000

    - Add Enumeration ArrowDirection - Add JButton ArrowComboBoxButton - Add container ArrowedComboBox - Replace all JComboBox drived by two JButton by ArrowedComboBox - Remove all Next/Previous JButton, replaced by embedded ArrowComboBoxButton in ArrowedComboBox - Librarian : Remove all code managing the navigation in JComboBox, now embedded in ArrowComboBoxButton

---
+++ src/components/buttons/ArrowComboBoxButton.java
@@ -0,0 +1,68 @@
+/**
+ * 
+ */
+package components.buttons;
+
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+
+import commons.enums.ArrowDirection;
+
+/**
+ * @author Naeregwen
+ *
+ */
+public class ArrowComboBoxButton extends JButton implements ActionListener {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8072526756835497478L;
+	
+	ArrowDirection arrowDirection;
+	JComboBox<?> comboBox;
+	
+	/**
+	 * @param arrowDirection
+	 */
+	public ArrowComboBoxButton(ArrowDirection arrowDirection, JComboBox<?> comboBox) {
+		super();
+		this.arrowDirection = arrowDirection;
+		this.comboBox = comboBox;
+		setIcon(arrowDirection.getImageIcon());
+		setMargin(new Insets(0, 0, 0, 0));
+		addActionListener(this);
+	}
+
+	/**
+	 * @return the arrowDirection
+	 */
+	public ArrowDirection getArrowDirection() {
+		return arrowDirection;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		int selectedIndex = -1;
+		switch (arrowDirection) {
+		case NEXT:
+			if (comboBox.getItemCount() <= 0) return;
+			selectedIndex = comboBox.getSelectedIndex();
+			int nextIndex = selectedIndex == comboBox.getItemCount() - 1 ? 0 : selectedIndex + 1;
+			comboBox.setSelectedIndex(nextIndex);
+			break;
+		case PREVIOUS:
+			if (comboBox.getItemCount() <= 0) return;
+			selectedIndex = comboBox.getSelectedIndex();
+			int previousIndex = selectedIndex == 0 ? comboBox.getItemCount() - 1 : selectedIndex - 1;
+			comboBox.setSelectedIndex(previousIndex);
+			break;
+		}
+	}
+	
+	
+}
